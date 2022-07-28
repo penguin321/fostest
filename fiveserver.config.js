e51ebab2f3148f17bad3bec0032c2e2d8c838f19
penguin321 <67345594+penguin321@users.noreply.github.com>
2022-07-28 12:19:51 +0200

    Add files via upload

---
+++ fiveserver.config.js
@@ -0,0 +1,9 @@
+// fiveserver.config.js
+module.exports = {
+    highlight: true, // enable highlight feature
+    injectBody: true, // enable instant update
+    remoteLogs: false, // enable remoteLogs
+    remoteLogs: "yellow", // enable remoteLogs and use the color yellow
+    injectCss: true, // disable injecting css
+    navigate: true, // enable auto-navigation
+  };
